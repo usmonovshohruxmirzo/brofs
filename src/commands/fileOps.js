4ac93109c8ae5d824781818679b06835ceca3204
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-06-19 23:22:16 +0500

    refactor(commands): file ops implmented (read, create, remove, rename)

---
+++ src/commands/fileOps.js
@@ -0,0 +1,45 @@
+import fs from "fs/promises";
+import { existsSync } from "fs";
+import { handleError, printSuccess, resolvePath } from "../utils/helpers";
+
+const readFile = async (filePath) => {
+  try {
+    const path = resolvePath(filePath);
+    const content = await fs.readFile(path, "utf-8");
+    console.log(content);
+  } catch {
+    handleError("Failed to read file");
+  }
+};
+
+const createFile = async (filePath) => {
+  try {
+    const path = resolvePath(filePath);
+    await fs.writeFile(path, "");
+    printSuccess("File created");
+  } catch {
+    handleError("Failed to create file.");
+  }
+};
+
+const removeFile = async (filePath) => {
+  try {
+    const path = resolvePath(filePath);
+    await fs.unlink(path);
+    printSuccess("File deleted.");
+  } catch {
+    handleError("Failed to delete file.");
+  }
+};
+
+const renameFile = async (oldPath, newPath) => {
+  try {
+    const path = resolvePath(filePath);
+    await fs.rename(resolvePath(oldPath), resolvePath(newPath));
+    printSuccess("Renamed successfully.");
+  } catch {
+    handleError("Rename failed.");
+  }
+};
+
+export { readFile, createFile, removeFile, renameFile };
