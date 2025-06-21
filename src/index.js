ff7d0a7c1614235eabf52f4386ac2677b5602416
Usmonov Shohruxmirzo <usmonovshohruxmirzo@gmail.com>
2025-06-21 08:27:23 +0500

    refactor: streamline command handling and improve help output in CLI

--- src/index.js
+++ src/index.js
@@ -1,29 +1,81 @@
 #!/usr/bin/env node
-import { program } from "commander";
-import * as fileOps from "./commands/fileOps.js";
-import * as nav from "./commands/navigation.js";
-import * as osInfo from "./commands/osInfo.js";
-import * as hash from "./commands/hash.js";
-import * as zip from "./commands/compress.js";
+import {
+  readFile,
+  createFile,
+  removeFile,
+  renameFile,
+} from "./commands/fileOps.js";
+import { changeDir, listDir } from "./commands/navigation.js";
+import { showOSInfo } from "./commands/osInfo.js";
+import { calculateHash } from "./commands/hash.js";
+import { compressFile, decompressFile } from "./commands/compress.js";
 
-program
-  .name("brofs")
-  .description("🤖 brofs - The Bro File System CLI")
-  .version("1.0.0");
+const args = process.argv.slice(2);
+const [command, ...rest] = args;
 
-program.command("read <file>").action(fileOps.readFile);
-program.command("create <file>").action(fileOps.createFile);
-program.command("delete <file>").action(fileOps.removeFile);
-program.command("rename <old> <new>").action(fileOps.renameFile);
+switch (command) {
+  case "read":
+    await readFile(rest[0]);
+    break;
+  case "create":
+    await createFile(rest[0]);
+    break;
+  case "delete":
+    await removeFile(rest[0]);
+    break;
+  case "rename":
+    await renameFile(rest[0], rest[1]);
+    break;
+  case "cd":
+    await changeDir(rest[0]);
+    break;
+  case "ls":
+    await listDir();
+    break;
+  case "os":
+    await showOSInfo();
+    break;
+  case "hash":
+    await calculateHash(rest[0]);
+    break;
+  case "zip":
+    await compressFile(rest[0], rest[1]);
+    break;
+  case "unzip":
+    await decompressFile(rest[0], rest[1]);
+    break;
+  case "-v":
+    console.log("v1.0.0");
+    break;
+  case "--help":
+    console.log(`
+██████╗ ██████╗  ██████╗ ███████╗███████╗
+██╔══██╗██╔══██╗██╔═══██╗██╔════╝██╔════╝
+██████╔╝██████╔╝██║   ██║█████╗  ███████╗
+██╔══██╗██╔══██╗██║   ██║██╔══╝  ╚════██║
+██████╔╝██║  ██║╚██████╔╝██║     ███████║
+╚═════╝ ╚═╝  ╚═╝ ╚═════╝ ╚═╝     ╚══════╝
+                                         
+🤖 brofs - The Bro File System CLI
 
-program.command("cd <dir>").action(nav.changeDir);
-program.command("ls").action(nav.listDir);
+Usage:
+  brofs ls                   List directory contents
+  brofs cd <dir>             Change directory
+  brofs read <file>          Read file content
+  brofs create <file>        Create an empty file
+  brofs delete <file>        Delete a file
+  brofs rename <old> <new>   Rename a file
+  brofs hash <file>          Print SHA256 hash
+  brofs zip <in> <out>       Gzip compress
+  brofs unzip <in> <out>     Gzip decompress
+  brofs os                   Show OS info
+  brofs -v                   Show Version
+  brofs -h                   Help
+    `);
+    break;
 
-program.command("os").action(osInfo.showOSInfo);
-
-program.command("hash <file>").action(hash.calculateHash);
-
-program.command("zip <input> <output>").action(zip.compressFile);
-program.command("unzip <input> <output>").action(zip.decompressFile);
-
-program.parse(process.argv);
+  default:
+    console.log(
+      `❌ Unknown command: "${command}". Use --help to see available commands.`
+    );
+}
